60e30ec4d8f503a91a91fb8d46f12396bcfcf861
sirius.se <sirius.se@ccpgames.com>
2019-05-10 14:47:55 +0800

    add test file

--- convert.py
+++ convert.py
@@ -4,5 +4,10 @@ import pprint
 
 p = pprint.PrettyPrinter(indent=2)
 
-with open("postman.jmx", "rb+") as fp:
-    p.pprint(xmltodict.parse(fp))
+# with open("postman.jmx", "rb+") as fp:
+#     p.pprint(xmltodict.parse(fp, dict_constructor=dict))
+
+with open("test.json", "r") as fp:
+    d = json.load(fp)
+    d["root"] = "root"
+    p.pprint(xmltodict.unparse(d, full_document=False, pretty=True))
